b82e87e657f66113ed0508ed68c6106bc7b014c8
ChristianJuanRodriguez535 <chrsitianjuanrodriguez535@gmail.com>
2024-11-04 22:16:26 -0600

    tercer ejercicio

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 //& Ejercicio 1: User profile 
 
 //* Write a program that prompts a user for their data: username,age, and a list of their favorite movies. Store the information and then showcase it in the console. 
@@ -45,16 +46,20 @@ function dataUsuario() {
 
 dataUsuario ();
 
-//& Problema 2: Highest number 
+//& Ejercicio 2: Highest number 
 
 //* Write a program that asks for 10 numbers. Using logical operators and any other JavaScript functions/structures you've seen before, determine an output the highst of those numbers
 
+
+numeroMasAlto();
 function numeroMasAlto(){
 
     const numeros = [];
     for (let i = 0; i < 10; i++){
         
-        const numerosIngresados = parseFloat(prompt(`Ingresa los números por favor ${i + 1}:`));
+        //*ciclo par ingresar los 10   numeros de 1 en 1
+
+        const numerosIngresados = parseFloat(prompt(`Ingresa 10 números de uno en uno por favor (${i + 1}):`));
 
         numeros.push(numerosIngresados); //&Almacena los números en el arreglo
     }
@@ -71,4 +76,43 @@ function numeroMasAlto(){
     console.log(`El número más algo es: ${encontrarMasAlto}`);
 }
 
-const numeros = [1,2,3,4,5,6,7,8,9,10];
+//& Ejercicio 3:  Alarm
+
+//*Write a program that asks a user for the amount of seconds needed until an alarm (message) is executed alongside a text to show once those seconds have passed in real time.
+
+//*Result example: "Time for bed after 10 seconds".
+iniciarAlarma();
+
+function iniciarAlarma(){
+
+    let segundos = parseInt(prompt("¿Cuantos segundos faltan para la alarma"));
+
+    let mensaje = prompt("¿Qué mensaje quieres mostrar cuando termine el tiempo?");
+
+    if (isNaN(segundos)|| segundos <= 0){
+
+        console.log("Por favor, ingresa un numero válido");
+        return;
+    }
+
+    console.log(`La alarma comenzara en ${segundos} segundos `);
+
+    let tiempoRestante = segundos; //& Bucle simple para contar los segundos 
+
+    let temporizador = setInterval(() => {
+        tiempoRestante--;//* se resta 1 seg al tiempo restante 
+
+        if (tiempoRestante > 0 ){
+            console.log(`Faltan ${tiempoRestante} segundos ...`);
+        } else {
+           
+            //& Cuando el tiempo se acaba, se muestra el mensaje y se detiene el temporizador
+            console.log(`Tiempo cumplido: ${mensaje}`);
+
+            clearInterval(temporizador);
+        }
+
+
+    }, 1000); 
+
+}
